7027c28197c75aa1935e2b9e98a6ef1d70689da4
SangWon7242 <gotkdqja@gmail.com>
2025-08-26 21:50:58 +0900

    업로드 된 파일의 메타 정보가 올바르게 저장

--- src/main/java/com/sbs/tutorial/app1/boudedContext/app/fileUpload/service/GenFileService.java
+++ src/main/java/com/sbs/tutorial/app1/boudedContext/app/fileUpload/service/GenFileService.java
@@ -3,7 +3,9 @@ package com.sbs.tutorial.app1.boudedContext.app.fileUpload.service;
 import com.sbs.tutorial.app1.boudedContext.app.article.entity.Article;
 import com.sbs.tutorial.app1.boudedContext.app.fileUpload.entity.GenFile;
 import com.sbs.tutorial.app1.boudedContext.app.fileUpload.repository.GenFileRepository;
+import com.sbs.tutorial.app1.util.Util.Util;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,15 +23,18 @@ public class GenFileService {
     for (String inputName : fileMap.keySet()) {
       MultipartFile multipartFile = fileMap.get(inputName);
 
-      String typeCode = "common"; // 파일 타입
-      String type2Code = "contentImg"; // 세부 타입(write.html에 있는 name)
-      String fileExt = "jpg"; // 파일 확장자
-      String fileExtTypeCode = "img"; // 확장 타입
-      String fileExtType2Code = "jpg"; // 확장 타입
-      int fileNo = 1; // 파일 번호
-      int fileSize = 1000; // 파일 크기
-      String fileDir = "article/2025/08/26"; // 저장 디렉토리
-      String originFileName = "???"; // 원본 파일명
+      String[] inputNameBits = inputName.split("__"); // common__contentImg__1" : ["common", "contentImg", "1"]
+
+
+      String typeCode = inputNameBits[0]; // 파일 타입
+      String type2Code = inputNameBits[1]; // 세부 타입(write.html에 있는 name)
+      String originFileName = multipartFile.getOriginalFilename(); // 원본 파일명
+      String fileExt = Util.file.getExt(originFileName); // 파일 확장자
+      String fileExtTypeCode = Util.file.getFileExtTypeCodeFromFileExt(fileExt); // 확장 타입
+      String fileExtType2Code = Util.file.getFileExtType2CodeFromFileExt(fileExt);; // 확장 타입
+      int fileNo = Integer.parseInt(inputNameBits[2]); // 파일 번호
+      int fileSize = (int)multipartFile.getSize(); // 파일 크기
+      String fileDir = relTypeCode + "/" + Util.date.getCurrentDateFormatted("yyyy_MM_dd");
 
       GenFile genFile = GenFile.builder()
           .relTypeCode(relTypeCode)
