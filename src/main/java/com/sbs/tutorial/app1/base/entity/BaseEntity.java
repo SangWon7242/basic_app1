8ed6de1eaabc5fc48a0f8f3f488da96cfcadcd4a
SangWon7242 <gotkdqja@gmail.com>
2025-08-28 21:15:27 +0900

    추가 정보를 위해 extra 변수 추가

--- src/main/java/com/sbs/tutorial/app1/base/entity/BaseEntity.java
+++ src/main/java/com/sbs/tutorial/app1/base/entity/BaseEntity.java
@@ -1,9 +1,7 @@
 package com.sbs.tutorial.app1.base.entity;
 
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -13,6 +11,8 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
@@ -31,6 +31,10 @@ public class BaseEntity {
   private LocalDateTime createDate; // LocalDateTime.NOW() 가 알아서 들어감
   @LastModifiedDate
   private LocalDateTime modifyDate;
+
+  @Transient // 아래 필드가 DB 필드가 되는 것을 막는다.
+  @Builder.Default
+  private Map<String, Object> extra = new LinkedHashMap<>();
 
   public BaseEntity(Long id) {
     this.id = id;
