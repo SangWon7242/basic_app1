4e1f5eea4fad798881be32256884debfd6f204e6
SangWon7242 <gotkdqja@gmail.com>
2025-08-28 19:20:37 +0900

    게시물 작성시 로컬에 파일 저장

--- src/main/java/com/sbs/tutorial/app1/boudedContext/app/fileUpload/service/GenFileService.java
+++ src/main/java/com/sbs/tutorial/app1/boudedContext/app/fileUpload/service/GenFileService.java
@@ -1,14 +1,15 @@
 package com.sbs.tutorial.app1.boudedContext.app.fileUpload.service;
 
+import com.sbs.tutorial.app1.base.AppConfig;
 import com.sbs.tutorial.app1.boudedContext.app.article.entity.Article;
 import com.sbs.tutorial.app1.boudedContext.app.fileUpload.entity.GenFile;
 import com.sbs.tutorial.app1.boudedContext.app.fileUpload.repository.GenFileRepository;
 import com.sbs.tutorial.app1.util.Util.Util;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.Map;
 
 @Service
@@ -23,8 +24,9 @@ public class GenFileService {
     for (String inputName : fileMap.keySet()) {
       MultipartFile multipartFile = fileMap.get(inputName);
 
-      String[] inputNameBits = inputName.split("__"); // common__contentImg__1" : ["common", "contentImg", "1"]
+      if(multipartFile.isEmpty()) continue;
 
+      String[] inputNameBits = inputName.split("__"); // common__contentImg__1" : ["common", "contentImg", "1"]
 
       String typeCode = inputNameBits[0]; // 파일 타입
       String type2Code = inputNameBits[1]; // 세부 타입(write.html에 있는 name)
@@ -51,6 +53,17 @@ public class GenFileService {
           .build();
 
       genFileRepository.save(genFile);
+
+      String filePath = AppConfig.GEN_FILE_DIR_PATH + "/" + fileDir + "/" + genFile.getFileName();
+
+      File file = new File(filePath);
+      file.getParentFile().mkdirs();
+
+      try {
+        multipartFile.transferTo(file); // 파일 저장
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     }
   }
 }
